339c0dc82e299b7486520bf19e40124bd337c9bb
lpszl <48978323+lpszl@users.noreply.github.com>
2019-03-27 14:41:23 +0800

    Update Work.cpp

--- Work.cpp
+++ Work.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 int main()
 {
-	int n, m;
+	int n, m;// n表示行数，m表示列数
 	cin >> n >> m;
 	while (n > 100 || n < 1 || m < 1 || m > 100) {
 		cout << "Incorrect mesh size." << endl;
 		cin >> n >> m;
 	}
+	// 创建记录输出顺序的二维数组
 	string **mesh = new string*[n];
 	for (int i = 0; i < n; i++) {
 		mesh[i] = new string[m];
 	}
 	string s;
-	int total = n * m;
+	int total = n * m;// 总的输入个数
 	while (cin >> s)
 	{
+		// 判断输入的无人机探测的字符串长度和探测网格大小是否一致，
 		if (s.length() != total) {
 			cout << "Data mismatch" << endl;
 			continue;
